6ba50fbaa035b0ff02a36b306cdbdc58c82de5ad
Anton Krymouski <akrymouski@bee2code.com>
2025-11-12 20:28:44 +0100

    logging added

--- Infrastructure/RpcClients/AuthRpcClient.cs
+++ Infrastructure/RpcClients/AuthRpcClient.cs
@@ -20,17 +20,18 @@ public class AuthRpcClient : IAuthRpcClient
             { "email", email },
             { "password", password }
         });
-        var response = await _rpcClient.SendAsync<LoginResult>(
-                new RpcRequest(
-                    id: new RpcId(Guid.NewGuid().ToString()),
-                    "Login",
-                    parameters
-                )
+        var requestId = new RpcId(Guid.NewGuid().ToString());
+        Console.WriteLine("Request ID {0}", requestId);
+        var request = new RpcRequest(
+            id: requestId,
+            "Login",
+            parameters
         );
+        var response = await _rpcClient.SendAsync<LoginResult>(request);
         
         if(response.HasError)
             return RpcResult<LoginResult>.Failure(response.Error?.Message);
-
+        Console.WriteLine("Response ID: {0}", response.Id);
         return string.IsNullOrWhiteSpace(response.Result.Message)
             ? RpcResult<LoginResult>.Success(response.Result)
             : RpcResult<LoginResult>.Failure(response.Result.Message);
